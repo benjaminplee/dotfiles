13044badc613c8ed05b2f2422ead78dd947cdba7
Benjamin P Lee <leebenjp@gmail.com>
2012-01-11 15:12:53 -0600

    pulled out boostrap include file

---
+++ bootstrap_include.sh
@@ -0,0 +1,22 @@
+function die()
+{
+    echo "${@}"
+    exit 1
+}
+
+function make_old()
+{
+  for i in $* ; do
+    if [[ ( -e $i ) || ( -h $i ) ]]; then
+      echo "${i} has been renamed to ${i}.old"
+      mv "${i}" "${i}.old.`date +%Y-%m-%d--%H-%M-%S`" || die "Could not move ${i} to ${i}.old"
+    fi
+  done
+}
+
+function rm_link()
+{
+  rm $1
+  ln -s $2 $1 || die "Could not symlink: ${1}"
+}
+
--- bootstrap_janus.sh
+++ bootstrap_janus.sh
@@ -1,29 +1,15 @@
-# taken from https://raw.github.com/carlhuda/janus/master/bootstrap.sh
+# adapted from https://raw.github.com/carlhuda/janus/master/bootstrap.sh
 
-function die()
-{
-    echo "${@}"
-    exit 1
-}
+make_old $HOME/.vim $HOME/.vimrc $HOME/.gvimrc
 
-# Add <strong>.old</strong> to any existing Vim file in the home directory
-for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do
-  if [[ ( -e $i ) || ( -h $i ) ]]; then
-    echo "${i} has been renamed to ${i}.old"
-    mv "${i}" "${i}.old" || die "Could not move ${i} to ${i}.old"
-  fi
-done
-
-# Clone Janus into .vim
 git clone https://github.com/carlhuda/janus.git $HOME/.vim \
   || die "Could not clone the repository to ${HOME}/.vim"
 
-# Run rake inside .vim
 cd $HOME/.vim || die "Could not go into the ${HOME}/.vim"
 rake || die "Rake failed."
 
-rm $HOME/.vimrc.before
-ln -s $PWD/.vimrc.before $HOME/.vimrc.before
+make_old $HOME/.vimrc.before
+rm_link $PWD/.vimrc.before $HOME/.vimrc.before
 
-rm $HOME/.vimrc.after
-ln -s $PWD/.vimrc.after $HOME/.vimrc.after
+make_old $HOME/.vimrc.after
+rm_link $PWD/.vimrc.after $HOME/.vimrc.after
