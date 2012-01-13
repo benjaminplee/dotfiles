2988a902ffeb273f0c04877e79e40d7a0289ab28
Benjamin P Lee <yardspon@gmail.com>
2012-01-13 12:14:52 -0600

    pulled out method

--- windows.ps1
+++ windows.ps1
@@ -1,17 +1,31 @@
 # Command to allow locally created scripts to run, enforce signing on remote
 # set-executionpolicy remotesigned
 
+function make_old([string]$target) {
+	If (Test-Path $target) {
+			If (Is_Link($target)) {
+				"${target} file already present as link; removing"
+				del $target
+			}
+			Else {
+				"${target} already present; moving to ${target}.old"
+				$date_string = Get-Date -Format "%y-%m-%d--%H-%M-%s" 
+				mv $target "${target}.${date_string}"
+			}
+	}
+}
+
+function Is_Link([string]$path) {
+  $file = Get-Item $path -Force -ea 0
+  return [bool]($file.Attributes -band [IO.FileAttributes]::ReparsePoint)
+}
+
 "Bootstrapping Dot Files in Windows"
 "----------------------------------"
 
 ########## powershell profile ###########
 "Bootstrapping powershell profile"
 "--------------------------------"
-
-function Is_Link([string]$path) {
-  $file = Get-Item $path -Force -ea 0
-  return [bool]($file.Attributes -band [IO.FileAttributes]::ReparsePoint)
-}
 
 $profile_folder = [IO.Path]::GetDirectoryName($Profile)
 
@@ -20,17 +34,7 @@ If ( !( Test-Path $profile_folder ) ) {
     mkdir -p $profile_folder
 }
 Else {
-	If (Test-Path $Profile) {
-			If (Is_Link($profile)) {
-				"Profile already present as link; removing"
-				del $Profile
-			}
-			Else {
-				"Profile already present; moving to ${profile}.old"
-				$date_string = Get-Date -Format "%y-%m-%d--%H-%M-%s" 
-				mv $profile "${profile}.${date_string}"
-			}
-	}
+	make_old($profile)
 }
 
 "Creating link to new powershell profile"
