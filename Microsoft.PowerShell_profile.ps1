ed234a8e8175ef758c08fc6236f90e404f9fa59b
Benjamin Lee <benjaminplee@users.noreply.github.com>
2015-02-19 12:46:13 -0600

    Added git aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,8 +2,6 @@ function rmrf {
     rm -Recurse -Force $Args
 }
 
-Set-Alias gitk gitk.cmd
-
 function prompt {
     Write-Host("" + $(get-location) + ">") -nonewline -ForegroundColor Yellow
     return " "
@@ -17,4 +15,16 @@ function insanity {
   do { iex $Args } while ($?)
 }
 
-cd $home
+function restartdb { NET STOP /Y MSSQLSERVER ; NET START MSSQLSERVER }
+Set-Alias time Measure-Command
+ 
+function story ($story_number) { $env:story_number = $story_number }
+function devs { $env:devs_working = $args}
+function ci { $msg = "$($env:story_number) - $($args) ($($env:devs_working))" ; git ci -m $msg}
+function cia { $msg = "$($env:story_number) - $($args) ($($env:devs_working))" ; git add . ; git ci -m $msg}
+ 
+function add { git add $Args }
+function st { git st }
+
+function d {cd ..}
+function u {cd ..}
